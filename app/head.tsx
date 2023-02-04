3e6790c0562a6435fef026ae3d36b071a147c2dd
beiku <beiku@naver.com>
2023-02-04 10:34:54 +0900

    user link view

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <head>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='description' content='Clone Linktree with Next.js 13 and TailwindCSS' />
+      <title>Beiku Link</title>
+      <link rel='icon' href='/favicon.ico' />
+    </head>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'image.xportsnews.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
